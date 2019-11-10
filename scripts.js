31d32d101a448fcb69be6fed3d78e774e0066467
Anthony <anthonydang112@gmail.com>
2019-11-10 13:23:03 -0500

    Added some fonts to the messages and fake paypal, added a message that appears after clicking the paypal element, allowed the iframe to appear after a set amount of time after the paypal element click

--- scripts.js
+++ scripts.js
@@ -3,11 +3,17 @@ console.log("Scripts Loaded");
 
 const div_login_box = document.getElementById('login-box');
 const div_pay_pal_logo = document.getElementById('pay-pal-logo');
-const div_main_page = document.getElementById("email-box");
+const div_main_page = document.getElementById('main-page');
+const div_warning_message = document.getElementById('warning-message');
 
 function youClicked(){
     console.log("clicked");
     div_login_box.classList.add("shift-away");
     div_pay_pal_logo.classList.add("fade-away");
-    div_main_page.style.display = "block";
+    div_warning_message.classList.add("appear-warning-message");
+    div_main_page.classList.add("appear-main-page");
 }
+
+//sets these  to hidden on startup of page
+div_main_page.style.visibility = "hidden";
+div_warning_message.style.visibility = "hidden";
