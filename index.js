cc0b46b623a7e9d3eeb253456ed035486681b9bb
Dominic Humber <dhumber2017@gmail.com>
2019-11-11 04:25:46 -0500

    clean up, change to the email body

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ console.log("Scripts Loaded");
 const startup = document.getElementById("startup");
 const main = document.getElementById("main");
 
+
 function fade() {
     console.log("clicked");
     startup.classList.add("fade-away");
+    main.classList.remove("hidden");
+    setTimeout(() => {
+        startup.classList.add("hidden");
+        document.querySelector('html').classList.add("background");
+    },2000);
     main.classList.add("appear-main-page");
+
 }
 
-
-//sets these  to hidden on startup of page
 main.style.visibility = "hidden";
